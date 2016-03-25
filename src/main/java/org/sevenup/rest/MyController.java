2930ba25b99c8437b2b766cde076bf3776b2c842
nonumber1989 <steven.xu01@sap.com>
2016-03-25 09:26:57 +0800

    make some changes

--- src/main/java/org/sevenup/rest/MyController.java
+++ src/main/java/org/sevenup/rest/MyController.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.sevenup;
+package org.sevenup.rest;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
