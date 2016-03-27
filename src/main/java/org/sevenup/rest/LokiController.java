6de6118a968957a03dcbde3886c19c95517a13f0
nonumber1989 <nonumber1989@gmail.com>
2016-03-27 16:27:58 +0800

    add jpa

--- src/main/java/org/sevenup/rest/LokiController.java
+++ src/main/java/org/sevenup/rest/LokiController.java
@@ -1,14 +1,21 @@
 package org.sevenup.rest;
 
+import java.util.List;
+
+import org.sevenup.domain.Note;
+import org.sevenup.repository.NoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LokiController {
-
+	@Autowired
+	private NoteRepository noteRepository;
 	@RequestMapping("/")
-	public String hello() {
-		return "Hello World!";
+	public List<Note> getNotes() {
+		List<Note> notes = this.noteRepository.findAll();
+		return notes;
 	}
 
 }
