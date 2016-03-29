e5c0819037ae8d8e8fd620aaf494bf90733da90a
nonumber1989 <nonumber1989@gmail.com>
2016-03-29 22:51:16 +0800

    add error handler

---
+++ src/main/java/org/sevenup/rest/LokiErrorController.java
@@ -0,0 +1,49 @@
+package org.sevenup.rest;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@RestController
+@RequestMapping("/error")
+public class LokiErrorController implements ErrorController {
+
+	private final ErrorAttributes errorAttributes;
+
+	@Autowired
+	public LokiErrorController(ErrorAttributes errorAttributes) {
+		this.errorAttributes = errorAttributes;
+	}
+
+	@Override
+	public String getErrorPath() {
+		return "/error";
+	}
+
+	@RequestMapping
+	public Map<String, Object> error(HttpServletRequest request) {
+		Map<String, Object> body = getErrorAttributes(request, getTraceParameter(request));
+		return body;
+	}
+
+	private boolean getTraceParameter(HttpServletRequest request) {
+		String parameter = request.getParameter("trace");
+		if (parameter == null) {
+			return false;
+		}
+		return !"false".equals(parameter.toLowerCase());
+	}
+
+	private Map<String, Object> getErrorAttributes(HttpServletRequest request, boolean includeStackTrace) {
+		RequestAttributes requestAttributes = new ServletRequestAttributes(request);
+		return errorAttributes.getErrorAttributes(requestAttributes, includeStackTrace);
+	}
+}
