fe38457d6fed2e43f8dcd918cfe2148de7f01ff1
Steven Xu <nonumber1989@gmail.com>
2016-04-21 00:27:06 +0800

    little changes

--- src/main/java/org/sevenup/rest/ArtworkController.java
+++ src/main/java/org/sevenup/rest/ArtworkController.java
@@ -1,46 +1,46 @@
-package org.sevenup.rest;
-
-import java.util.Optional;
-
-import javax.validation.Valid;
-
-import org.sevenup.common.exception.ResourceNotFoundException;
-import org.sevenup.domain.Artwork;
-import org.sevenup.service.ArtworkService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefaults;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/artworks")
-public class ArtworkController {
-	@Autowired
-	private ArtworkService artworkService;
-	@RequestMapping(method = RequestMethod.GET)
-	public Page<Artwork> getPageableArtworks(
-			@PageableDefaults(pageNumber = 0, value = 10, sort = { "id" }) Pageable pageable) {
-		Page<Artwork> pageArtwork = artworkService.findByPageable(pageable);
-		return pageArtwork;
-	}
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public Artwork getUserById(@PathVariable Long id) {
-		Optional<Artwork> artwork = Optional.ofNullable(artworkService.findById(id));
-		return artwork.orElseThrow(ResourceNotFoundException::new);
-	}
-	
-	@RequestMapping(method = RequestMethod.POST)
-	public Artwork createUser(@Valid @RequestBody Artwork artwork) {
-		return artworkService.save(artwork);
-	}
-	
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public Artwork delete(@PathVariable Long id) {
-		 return artworkService.delete(id);
-	}
-}
+//package org.sevenup.rest;
+//
+//import java.util.Optional;
+//
+//import javax.validation.Valid;
+//
+//import org.sevenup.common.exception.ResourceNotFoundException;
+//import org.sevenup.domain.Artwork;
+//import org.sevenup.service.ArtworkService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.web.PageableDefaults;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/artworks")
+//public class ArtworkController {
+//	@Autowired
+//	private ArtworkService artworkService;
+//	@RequestMapping(method = RequestMethod.GET)
+//	public Page<Artwork> getPageableArtworks(
+//			@PageableDefaults(pageNumber = 0, value = 10, sort = { "id" }) Pageable pageable) {
+//		Page<Artwork> pageArtwork = artworkService.findByPageable(pageable);
+//		return pageArtwork;
+//	}
+//	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+//	public Artwork getUserById(@PathVariable Long id) {
+//		Optional<Artwork> artwork = Optional.ofNullable(artworkService.findById(id));
+//		return artwork.orElseThrow(ResourceNotFoundException::new);
+//	}
+//	
+//	@RequestMapping(method = RequestMethod.POST)
+//	public Artwork createUser(@Valid @RequestBody Artwork artwork) {
+//		return artworkService.save(artwork);
+//	}
+//	
+//	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+//	public Artwork delete(@PathVariable Long id) {
+//		 return artworkService.delete(id);
+//	}
+//}
--- src/main/java/org/sevenup/service/ArtworkServiceHandler.java
+++ src/main/java/org/sevenup/service/ArtworkServiceHandler.java
@@ -1,52 +1,52 @@
-
-package org.sevenup.service;
-
-import org.sevenup.domain.Artwork;
-import org.sevenup.repository.ArtworkRepository;
-import org.sevenup.repository.springdata.ArtworkDefaultRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
-public class ArtworkServiceHandler implements ArtworkService {
-	@Autowired
-	private ArtworkRepository artworkRepository;
-	@Autowired
-	private ArtworkDefaultRepository JpaRepository;
-
-	@Override
-	public Page<Artwork> findByPageable(Pageable pageable) {
-		return JpaRepository.findAll(pageable);
-	}
-
-	@Override
-	public Artwork findById(Long id) {
-		return (Artwork) JpaRepository.findOne(id);
-	}
-
-	@Override
-	public boolean exists(Long id) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public long count() {
-		return artworkRepository.count();
-	}
-
-	@Override
-	@Transactional
-	public Artwork delete(Long id) {
-		return artworkRepository.delete(id);
-	}
-
-	@Override
-	public Artwork save(Artwork entity) {
-		return JpaRepository.save(entity);
-	}
-
-}
+//
+//package org.sevenup.service;
+//
+//import org.sevenup.domain.Artwork;
+//import org.sevenup.repository.ArtworkRepository;
+//import org.sevenup.repository.springdata.ArtworkDefaultRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@Service
+//public class ArtworkServiceHandler implements ArtworkService {
+//	@Autowired
+//	private ArtworkRepository artworkRepository;
+//	@Autowired
+//	private ArtworkDefaultRepository JpaRepository;
+//
+//	@Override
+//	public Page<Artwork> findByPageable(Pageable pageable) {
+//		return JpaRepository.findAll(pageable);
+//	}
+//
+//	@Override
+//	public Artwork findById(Long id) {
+//		return (Artwork) JpaRepository.findOne(id);
+//	}
+//
+//	@Override
+//	public boolean exists(Long id) {
+//		// TODO Auto-generated method stub
+//		return false;
+//	}
+//
+//	@Override
+//	public long count() {
+//		return artworkRepository.count();
+//	}
+//
+//	@Override
+//	@Transactional
+//	public Artwork delete(Long id) {
+//		return artworkRepository.delete(id);
+//	}
+//
+//	@Override
+//	public Artwork save(Artwork entity) {
+//		return JpaRepository.save(entity);
+//	}
+//
+//}
